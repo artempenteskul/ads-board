bd93d3fc8462be2c75009dccf95d81c11c2d3994
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-12-09 22:11:32 +0200

    added right env variables

--- base/settings.py
+++ base/settings.py
@@ -55,17 +55,13 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': os.environ.get('DB_ENGINE'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT')
     },
-    '3default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
