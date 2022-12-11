b55ed4948d6c1a95c3c3a03ec3c268029b0cd6b6
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-12-11 20:12:12 +0200

    added about page

--- advert/urls.py
+++ advert/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import home, about, index
+from .views import home, about, index, other_page
 
 
 app_name = 'advert'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('<str:page>/', other_page, name='other'),
+    path('/', index, name='index'),
     path('home/', home, name='home'),
     path('about/', about, name='about')
 ]
--- advert/views.py
+++ advert/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def other_page(request, page):
+    try:
+        template = get_template(f'{page}.html')
+    except TemplateDoesNotExist:
+        raise Http404
+
+    return render(request, template)
 
 
 def home(request):
