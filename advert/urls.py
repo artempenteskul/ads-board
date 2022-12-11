2a1a9975b644c3722949d42de2ccac1ebe63471a
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-12-11 19:58:58 +0200

    added route for index

--- advert/urls.py
+++ advert/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import home, about
+from .views import home, about, index
 
 
 app_name = 'advert'
 
 urlpatterns = [
+    path('', index, name='index'),
     path('home/', home, name='home'),
     path('about/', about, name='about')
 ]
