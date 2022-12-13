47cb57397a359005539399ef463c6204535b5ce1
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-12-13 22:02:46 +0200

    added login settings vars

--- base/settings.py
+++ base/settings.py
@@ -101,6 +101,9 @@ STATICFILES_DIRS = [
     'static',
 ]
 
+LOGIN_URL = 'advert:login'
+LOGIN_REDIRECT_URL = 'advert:index'
+
 AUTH_USER_MODEL = 'advert.AdvUser'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
