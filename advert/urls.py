55d24dd4f9d7079fc3c54783f20f98f15db0be32
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-12-17 17:02:53 +0200

    added user deletion view

--- advert/urls.py
+++ advert/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import (
     index, other_page, profile,
     AdsBoardLoginView, AdsBoardLogoutView, AdsBoardPasswordChangeView,
-    ChangeUserInfoView, RegisterUserView, RegisterDoneView, user_activate
+    ChangeUserInfoView, RegisterUserView, RegisterDoneView, user_activate, DeleteUserView
 )
 
 app_name = 'advert'
@@ -14,6 +14,7 @@ urlpatterns = [
     path('profile/', profile, name='profile'),
     path('profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('password/change/', AdsBoardPasswordChangeView.as_view(), name='password_change'),
+    path('profile/delete/', DeleteUserView.as_view(), name='profile_delete'),
     path('register/activate/<str:sign>/', user_activate, name='register_activate'),
     path('register/done/', RegisterDoneView.as_view(), name='register_done'),
     path('register/', RegisterUserView.as_view(), name='register'),
--- advert/views.py
+++ advert/views.py
@@ -5,12 +5,14 @@ from django.template.loader import get_template
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import UpdateView, CreateView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 from django.core.signing import BadSignature
+from django.contrib import messages
 
 from .models import AdvUser
 from .forms import ChangeUserInfoForm, RegisterUserForm
@@ -63,6 +65,26 @@ class RegisterDoneView(TemplateView):
     template_name = 'register_done.html'
 
 
+class DeleteUserView(LoginRequiredMixin, DeleteView):
+    model = AdvUser
+    template_name = 'delete_user.html'
+    success_url = reverse_lazy('advert:index')
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        messages.add_message(request, messages.SUCCESS, 'User was deleted')
+        return super().post(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
+
+    
 def user_activate(request, sign):
     try:
         username = signer.unsign(sign)
