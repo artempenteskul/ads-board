1b3db6489948453cbbe6b8385d1213e16216f877
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-12-20 18:25:34 +0200

    added models for rubrics and super rubrics

--- advert/models.py
+++ advert/models.py
@@ -1,1 +1,25 @@
 from django.db import models
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=32, db_index=True, unique=True, verbose_name='Name')
+    order = models.SmallIntegerField(default=0, db_index=True, verbose_name='Order')
+    super_rubric = models.ForeignKey('SuperRubric', on_delete=models.PROTECT, null=True, blank=True, verbose_name='Super Rubric')
+
+
+class SuperRubricManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(super_rubric__isnull=True)
+
+
+class SuperRubric(Rubric):
+    objects = SuperRubricManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        proxy = True
+        ordering = ('order', 'name')
+        verbose_name = 'Super Rubric'
+        verbose_name_plural = 'Super Rubrics'
