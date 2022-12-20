5798258218882a8b57bffe926301af83b05d5f7d
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-12-20 18:32:52 +0200

    added rubrics model to admin file

--- advert/admin.py
+++ advert/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+
+from .models import SubRubric, SuperRubric
+
+
+class SubRubricInline(admin.TabularInline):
+    model = SubRubric
+
+
+class SuperRubricAdmin(admin.ModelAdmin):
+    exclude = ('super_rubric',)
+    inlines = (SubRubricInline,)
+
+
+admin.site.register(SuperRubric, SuperRubricAdmin)
