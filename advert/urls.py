268ebb004aac360628aba93965a425f8a2f6ccc3
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-12-20 18:46:00 +0200

    added by_rubric view

--- advert/urls.py
+++ advert/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from .views import by_rubric
+
 
 app_name = 'advert'
 
 urlpatterns = [
-
+    path('<int:pk>/', by_rubric, name='by_rubric'),
 ]
--- advert/views.py
+++ advert/views.py
@@ -1,1 +1,5 @@
 from django.shortcuts import render
+
+
+def by_rubric(request, pk):
+    pass
