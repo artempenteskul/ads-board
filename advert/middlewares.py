0dc7c43e4108b6b362b87a3c5c1d4633bb62d89f
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-12-21 17:06:01 +0200

    updated ads_board middleware

--- advert/middlewares.py
+++ advert/middlewares.py
@@ -4,4 +4,18 @@ from .models import SubRubric
 def ads_board_context_processor(request):
     context = dict()
     context['rubrics'] = SubRubric.objects.all()
+    context['keyword'] = ''
+    context['all'] = ''
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            context['keyword'] = '?keyword=' + keyword
+            context['all'] = context['keyword']
+    if 'page' in request.GET:
+        page = request.GET['page']
+        if page != '1':
+            if context['all']:
+                context['all'] += '&page=' + page
+            else:
+                context['all'] = '?page=' + page
     return context
