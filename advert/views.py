6b2a99f6dead3771d9a073bd00f7a3d0c46d8c3f
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-12-21 17:00:32 +0200

    added by_rubric view

--- advert/views.py
+++ advert/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from django.db.models import Q
+
+from .models import SubRubric, Advert
+from .forms import SearchForm
 
 
 def by_rubric(request, pk):
-    pass
+    rubric = get_object_or_404(SubRubric, pk=pk)
+    ads = Advert.objects.filter(is_active=True, rubric=pk)
+
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        q = Q(title__icontains=keyword) | Q(content__icontains=keyword)
+        ads = ads.filter(q)
+    else:
+        keyword = ''
+
+    form = SearchForm(initial={'keyword': keyword})
+    paginator = Paginator(ads, 2)
+
+    if 'page' in request.GET:
+        page_num = request.GET['page']
+    else:
+        page_num = 1
+
+    page = paginator.get_page(page_num)
+    context = {'rubric': rubric, 'page': page, 'ads': page.object_list, 'form': form}
+    return render(request, 'by_rubric.html', context)
