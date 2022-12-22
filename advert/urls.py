9739bb856a9068d72477bcc9b61e13cb46ad7781
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-12-22 13:28:13 +0200

    added url for add_new_ad

--- advert/urls.py
+++ advert/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import index, by_rubric, detail
+from .views import index, by_rubric, detail, add_new_ad
 
 
 app_name = 'advert'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('add/', add_new_ad, name='add_new_ad'),
     path('<int:rubric_pk>/<int:pk>/', detail, name='detail'),
     path('<int:pk>/', by_rubric, name='by_rubric'),
 ]
